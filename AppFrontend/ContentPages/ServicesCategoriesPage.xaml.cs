1cb950d0175e40f2180efe047dccca33780f3f59
Adelina Fatu <adelinamfatu@gmail.com>
2022-12-09 22:11:14 +0200

    Initial commit

--- AppFrontend/ContentPages/ServicesCategoriesPage.xaml.cs
+++ AppFrontend/ContentPages/ServicesCategoriesPage.xaml.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace AppFrontend.ContentPages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ServicesCategoriesPage : TabbedPage
+    public partial class ServicesCategoriesPage : Xamarin.Forms.TabbedPage
     {
         public ServicesCategoriesPage()
         {
             InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
     }
 }
