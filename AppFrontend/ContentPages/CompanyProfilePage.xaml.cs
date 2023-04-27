9f5062757292e325b67943aa253b7c8c85775e9b
Adelina Fatu <adelinamfatu@gmail.com>
2023-04-27 09:57:18 +0300

    Added uniform grid for functions

--- AppFrontend/ContentPages/CompanyProfilePage.xaml.cs
+++ AppFrontend/ContentPages/CompanyProfilePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using AppFrontend.Resources;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,43 @@ namespace AppFrontend.ContentPages
             {
                 this.BindingContext = globalService.Company;
             }
+        }
+
+        private void ChangePage(object sender, CheckedChangedEventArgs e)
+        {
+            if(page2Button.IsChecked == true)
+            {
+                name.IsVisible = false;
+                city.IsVisible = false;
+                street.IsVisible = false;
+                streetNumber.IsVisible = false;
+                building.IsVisible = false;
+                staircase.IsVisible = false;
+                apartmentNumber.IsVisible = false;
+                functionEntryGrid.IsVisible = true;
+                functionsUniformGrid.IsVisible = true;
+            }
+            else
+            {
+                name.IsVisible = true;
+                city.IsVisible = true;
+                street.IsVisible = true;
+                streetNumber.IsVisible = true;
+                building.IsVisible = true;
+                staircase.IsVisible = true;
+                apartmentNumber.IsVisible = true;
+                functionEntryGrid.IsVisible = false;
+                functionsUniformGrid.IsVisible = false;
+            }
+        }
+
+        private void AddFunctionToList(object sender, EventArgs e)
+        {
+            if (globalService.Company.Functions == null)
+            {
+                globalService.Company.Functions = new List<string>();
+            }
+            globalService.Company.Functions.Add(function.Text);
         }
     }
 }
