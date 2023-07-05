e61f48c74ae95668b495173be8fea1380526f625
Adelina Fatu <adelinamfatu@gmail.com>
2023-07-05 21:14:49 +0300

    Updated services search page to use the photos from IIS

--- AppFrontend/ContentPages/Client/SearchServicePage.xaml.cs
+++ AppFrontend/ContentPages/Client/SearchServicePage.xaml.cs
@@ -39,7 +39,8 @@ namespace AppFrontend.ContentPages
                 services.Add(new ServiceDTO
                 {
                     ID = service.ID,
-                    Name = service.Name
+                    Name = service.Name,
+                    Icon = service.Icon
                 });
             }
         }
